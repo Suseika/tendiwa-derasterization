4c09034a3565272655f5774e7036fed96c409484
Georgy Vlasov <wlasowegor@gmail.com>
2015-12-07 05:26:37 +0500

    Implement derasterization of a BoundedGridMask into a Polygon

--- src/main/kotlin/org/tendiwa/derasterization/polygons/PolygonDerasterization.kt
+++ src/main/kotlin/org/tendiwa/derasterization/polygons/PolygonDerasterization.kt
@@ -1,159 +1,84 @@
 package org.tendiwa.derasterization.polygons
 
 import org.tendiwa.derasterization.point
-import org.tendiwa.geometry.points.move
+import org.tendiwa.geometry.points.Point
 import org.tendiwa.geometry.polygons.Polygon
+import org.tendiwa.geometry.segments.Segment
+import org.tendiwa.geometry.segments.slope
+import org.tendiwa.graphs.neighbors.neighborsOf
+import org.tendiwa.graphs.trails.trail
+import org.tendiwa.graphs.vertices
 import org.tendiwa.grid.algorithms.buffers.inwardBuffer
-import org.tendiwa.grid.constructors.centeredGridRectangle
-import org.tendiwa.grid.dimensions.by
-import org.tendiwa.grid.masks.*
-import org.tendiwa.grid.metrics.GridMetric
+import org.tendiwa.grid.algorithms.buffers.kingBuffer
+import org.tendiwa.grid.algorithms.connectivity.connectivityComponents
+import org.tendiwa.grid.masks.BoundedGridMask
+import org.tendiwa.grid.masks.boundedBy
+import org.tendiwa.grid.masks.difference
+import org.tendiwa.grid.metrics.GridMetric.TAXICAB
 import org.tendiwa.grid.tiles.Tile
-import org.tendiwa.grid.tiles.isNear
-import org.tendiwa.grid.tiles.move
 import org.tendiwa.grid.tiles.neighbors
-import org.tendiwa.plane.directions.CardinalDirection
-import org.tendiwa.plane.directions.CardinalDirection.*
-import org.tendiwa.plane.directions.OrdinalDirection.*
-import org.tendiwa.plane.geometry.graphs.MutableGraph2D
-import org.tendiwa.plane.geometry.graphs.algorithms.minimumCycleBasis.MinimumCycleBasis
+import org.tendiwa.math.doubles.isInteger
+import org.tendiwa.plane.directions.CardinalDirection.E
+import org.tendiwa.plane.directions.CardinalDirection.S
+import org.tendiwa.plane.geometry.graphs.Graph2D
 import org.tendiwa.plane.geometry.graphs.algorithms.minimumCycleBasis.minimumCycleBasis
-import java.awt.Color
-import java.util.*
+import org.tendiwa.plane.geometry.graphs.constructors.Graph2D
 
-fun BoundedGridMask.derasterized(seed: Tile = findFloodStart()): Set<Polygon> {
-    val graph : MutableGraph2D = PolygonBorderGraph(this.polygonsBorders)
-    return graph.minimumCycleBasis.minimalCycles
-}
+val BoundedGridMask.derasterized: Set<Polygon>
+    get() =
+    connectivityComponents(metric = TAXICAB)
+        .map { component ->
+            component
+                .difference(component.inwardBuffer(1))
+                .kingBuffer(1)
+                .boundedBy(component.hull)
+        }
+        .flatMap { it.tiles }
+        .run(::segmentsBetweenNeighbors)
+        .run(::Graph2D)
+        .withConsecutiveEdgesCollapsed
+        .minimumCycleBasis
+        .minimalCycles
 
-fun PolygonBorderGraph(polygonsBorders: BoundedGridMask): MutableGraph2D {
-    val graph = MutableGraph2D()
-    val tilesToPoints = polygonsBorders
-        .tiles
-        .map { Pair(it, it.point)}
+private fun segmentsBetweenNeighbors(tiles: Iterable<Tile>): List<Segment> =
+    tiles
+        .map { Pair(it, it.point) }
+        .apply { assert(all { it.second.x.isInteger || it.second.y.isInteger }) }
         .toMap()
-    tilesToPoints.values.forEach { graph.addVertex(it) }
-    tilesToPoints.keys.forEach {
-        if (tilesToPoints.containsKey(it.move(E))) {
-            graph.addEdge(tilesToPoints[it], tilesToPoints[it.move (E)])
+        .let { tilesToPoints ->
+            tilesToPoints.keys
+                .flatMap { tile ->
+                    tile.neighbors(S, E)
+                        .tiles
+                        .filter { tilesToPoints.containsKey(it) }
+                        .map { neighbor -> Pair(tile, neighbor) }
+                }
+                .map {
+                    Segment(
+                        tilesToPoints[it.first]!!,
+                        tilesToPoints[it.second]!!
+                    )
+                }
         }
-        if (tilesToPoints.containsKey(it.move(S))) {
-            graph.addEdge(tilesToPoints[it], tilesToPoints[it.move(S)])
-        }
-    }
-    return graph
-}
 
-private val BoundedGridMask.polygonsBorders: BoundedGridMask
+private val Graph2D.withConsecutiveEdgesCollapsed: Graph2D
     get() {
-        val border = this.inwardBuffer(1).boundedBy(this.hull)
-        val innards = this.difference(this.inwardBuffer(1))
-        val excludedTilesMask = ExcludedTilesMask(border, innards)
-        val difference = border.difference(excludedTilesMask)
-        return difference
-    }
-
-/**
- * Mask of tiles that should be excluded form `original` mask in order for the
- * rest of the `original` mask to contain only tiles with 2 [GridMetric.TAXICAB]
- * neighbors.
- */
-private fun ExcludedTilesMask(
-    border: BoundedGridMask,
-    innards: BoundedGridMask
-): FiniteGridMask {
-    return FiniteGridMask(
-        LinkedHashSet<Tile>().apply {
-            val excluded3 = tilesExcludedBecauseOf3Knots(border, innards)
-            addAll(excluded3)
-            addAll(tilesExcludedBecauseOf4Knots(border, innards))
-        }
-    )
-}
-
-private fun tilesExcludedBecauseOf4Knots(
-    border: BoundedGridMask,
-    innards: BoundedGridMask
-): Collection<Tile> =
-    border.find4Knots().run {
-        ArrayList<Tile>().apply {
-            addAll(
-                this.filter {
-                    innards.run {
-                        contains(it.move(NW)) && contains(it.move(SE))
-                    }
-                }
-                    .flatMap { listOf(it.move(NW), it.move(SE)) }
-            )
-            addAll(
-                this.filter {
-                    innards.run {
-                        contains(it.move(NE)) && contains(it.move(SW))
-                    }
-                }
-                    .flatMap { listOf(it.move(NE), it.move(SW)) }
-            )
-        }
-    }
-
-private fun tilesExcludedBecauseOf3Knots(
-    border: BoundedGridMask,
-    innards: BoundedGridMask
-): List<Tile> {
-    return border.find3Knots().run {
-        this.map { knot -> Pair(knot, this.neighborOf(knot)) }
-            .filter { it.second != null }
-            .map { Adjacent3KnotsPair(it.first, it.second!!) }
-            .map { it.surroundingTiles }
-            .map { it.difference(innards) }
-            .flatMap { it.tiles }
-    }
-}
-
-private data class Adjacent3KnotsPair(val knot: Tile, val neighbor: Tile) {
-    init {
-        assert(knot.isNear(neighbor, GridMetric.KING))
-    }
-}
-
-private val Adjacent3KnotsPair.surroundingTiles: BoundedGridMask
-    get() = GridMask {
-        x, y ->
-        Tile(x, y).run {
-            this.isNear(knot, GridMetric.KING)
-                || this.isNear(neighbor, GridMetric.KING)
-        }
-    }
-        .boundedBy(centeredGridRectangle(knot, 3 by 3))
-
-private fun Set<Tile>.neighborOf(knot: Tile): Tile? =
-    if (contains(knot.move (1, 0))) {
-        knot.move(1, 0)
-    } else if (contains(knot.move(0, 1))) {
-        knot.move(0, 1)
-    } else {
-        null
-    }
-
-
-private fun BoundedGridMask.find3Knots(): Set<Tile> =
-    this.tiles.filter { this.numberOfNeighbors(it) > 2 }.toHashSet()
-
-private fun BoundedGridMask.find4Knots(): Set<Tile> =
-    this.tiles.filter { this.numberOfNeighbors(it) == 4 }.toHashSet()
-
-private fun GridMask.numberOfNeighbors(it: Tile): Int {
-    var number = 0
-    it.neighbors(GridMetric.TAXICAB)
-        .forEachTile { x, y ->
-            if (this.contains (x, y)) {
-                number++
+        val remainingVertices = vertices
+            .filter { !hasCollapseableEdges(it) }
+            .toSet()
+        return vertices
+            .first()
+            .let { anyVertex ->
+                trail(
+                    start = anyVertex,
+                    past = neighborsOf(anyVertex).first()
+                )
             }
-        }
-    assert(number <= 4)
-    return number
-}
+            .map { it.payload }
+            .filter { it in remainingVertices }
+            .run { Graph2D(Polygon(this)) }
+    }
 
-private fun findFloodStart(): Tile {
-    throw UnsupportedOperationException();
-}
+private fun Graph2D.hasCollapseableEdges(vertex: Point): Boolean =
+    edgesOf(vertex).map { it.slope }.distinct().size == 1
+
--- src/test/kotlin/org/tendiwa/derasterization/polygons/PolygonDerasterizationTest.kt
+++ src/test/kotlin/org/tendiwa/derasterization/polygons/PolygonDerasterizationTest.kt
@@ -1,2 +1,72 @@
 package org.tendiwa.derasterization.polygons
 
+import org.junit.Test
+import org.tendiwa.graphs.edges
+import org.tendiwa.grid.constructors.GridRectangle
+import org.tendiwa.grid.dimensions.by
+import org.tendiwa.grid.masks.*
+import org.tendiwa.grid.rectangles.hulls.GridRectangularHull
+import org.tendiwa.plane.geometry.graphs.constructors.Graph2D
+import kotlin.test.assertEquals
+
+class PolygonDerasterizationTest {
+    @Test fun collapsesContinuousEdgeChains() {
+        GridRectangle(10 by 10)
+            .derasterized
+            .first()
+            .apply { assertEquals(4, segments.size) }
+    }
+
+    @Test fun derasterizesGridMaskWithMultipleConnectivityComponents() {
+        val component1 = GridRectangle(10 by 10)
+        val component2 = component1.move(20, 20)
+        component1
+            .union(component2)
+            .boundedBy(GridRectangularHull(component1, component2))
+            .derasterized
+            .apply {
+                assertEquals(2, size)
+            }
+    }
+
+    @Test fun rejectsExtraTiles() {
+        val complex =
+            StringGridMask(
+                ".....###..",
+                ".....###..",
+                ".....###.#",
+                ".....##..#",
+                "..##..####",
+                "##########",
+                "#####.####",
+                ".####.####",
+                ".####.#.##",
+                "......####"
+            )
+        val simplified =
+            StringGridMask(
+                ".....###..",
+                ".....###..",
+                ".....###..",
+                "..........",
+                "......####",
+                ".####.####",
+                ".####.####",
+                ".####.####",
+                ".####.....",
+                ".........."
+            )
+        assertEquals(
+            Graph2D(simplified.derasterized).edges,
+            Graph2D(complex.derasterized).edges
+        )
+    }
+
+    @Test fun derasterizesEmptyMask() {
+        EmptyGridMask()
+            .boundedBy(GridRectangle(3 by 4))
+            .derasterized
+            .apply { assertEquals(0, size) }
+    }
+}
+
